a5f26523de0454d260c43b0a70aedcf47491f1d1
Eric Broadbent <ebroadbent@tooeleschools.org>
2018-09-07 09:20:05 -0600

    add unit one examples

---
+++ unit 1 exe.py
@@ -0,0 +1,44 @@
+print("this is a string")
+name = "Eric"
+print(type(name))
+fact = "my favorite game is WOW"
+message = "my name is "+  name+  " and I like "+ fact
+print(message)
+
+num = 8
+num2 = 14
+
+print(num+num2)
+
+answer = num-num2
+print (answer)
+
+word1 = input("enter a word")
+word2 = input("enter a word")
+word3 = input("enter a word")
+word4 = input("enter a word")
+word5 =input("enter a word")
+print("A", word1, word2, word5, "a", word3, "in the", word4 + ".")
+
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
