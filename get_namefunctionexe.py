2ea213c1d012764e19c8d8d93d37de78fde73a30
Eric Broadbent <ebroadbent@tooeleschools.org>
2018-09-13 09:53:12 -0600

    functuon example

---
+++ get_namefunctionexe.py
@@ -0,0 +1,52 @@
+#Eric Broadbent
+#9/18
+#get name function
+def get_name():
+    
+
+#ask user for name
+    name=input("what is your name")
+
+    
+    
+#display name
+    print("the name you entered was", name)
+
+
+print("this is our function")
+get_name()
+
+
+def areaOfCricle():
+    PI = 3.14159265
+#1 get a radius
+    x = input("what is the radius")
+
+#2 compute te area
+    radius = float(x)
+    area= radius*radius*PI
+#3 display information
+    print("the area of the cricle is: ",area)
+
+
+
+areaOfCricle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
