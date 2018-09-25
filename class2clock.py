83e246e021822555a82edaf215ef0a1cef424e2e
Eric Broadbent <ebroadbent@tooeleschools.org>
2018-09-25 14:40:49 -0600

    clock with alarm and inputs

--- class2clock.py
+++ class2clock.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import font
+import winsound
 import time
 import calendar
 import datetime
@@ -12,42 +13,30 @@ def current_time():
     current_minutes= minutes % 60
     hours = minutes // 60
     current_hour = hours % 24
-    if current_hour >=12:
-        tag="  PM"
-    else:
-        tag="  AM"
-    
     #set the time zone 
     current_hour = current_hour - 6
-    current_hour = current_hour%12
-    #make sure houer will be set to milatary time
-    if current_hour >= 24:
-        curent_hour = 1
-    #format the output
+    if current_hour >=12:
+        tag="PM"
+    else:
+        tag="AM"    
     timex = str(current_hour)+":"+ str(current_minutes)+":"+str( current_seconds)+tag
-    #xreturn the output
     return timex
 
+#winsound.Beep(540,8000)
 def quit(*args):
     root.destroy()
-    
 def show_time():
     time = current_time()
-    # Show the time
     txt.set(time)
-    # Trigger the tick after 1000ms
     root.after(1000, show_time)
 
-# Use tkinter lib for showing the clock
 root = Tk()
 root.attributes("-fullscreen", False)
 root.configure(background='Green')
 root.bind("x", quit)
 root.after(1000, show_time)
-
 fnt = font.Font(family='Helvetica', size=60, weight='bold')
 txt = StringVar()
 lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="Yellow", background="red")
 lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
-
 root.mainloop()
